fadffdebecc1b7fdd7ee603dc355e759d1e6e51c
ranawp <ranasaha4224#gmail.com>
2022-04-03 17:20:48 +0600

    start project

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Cart from '../Cart/Cart';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className='tshirt-container'>
+
+            </div>
+            <div className='cart-container'>
+                <Cart></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
